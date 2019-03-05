536f866633b8ca5d02bf988e16742b496f1db4d8
Payton Swick <payton@foolord.com>
2019-03-05 15:55:14 -0500

    Add spaceBar as alias to up arrow

--- src/controls.js
+++ src/controls.js
@@ -15,12 +15,16 @@ export function setUpKeyboardControls(
 	changeControlMode,
 	getPressingState
 ) {
+	// number is JS key code: https://keycode.info/
 	const upArrow = game.keyboard(38);
 	const leftArrow = game.keyboard(37);
 	const rightArrow = game.keyboard(39);
 	const modeKey = game.keyboard(77); // 'm'
+	const spaceBar = game.keyboard(32);
 	upArrow.press = () => changePressingState({ ...getPressingState(), up: true });
 	upArrow.release = () => changePressingState({ ...getPressingState(), up: false });
+	spaceBar.press = () => changePressingState({ ...getPressingState(), up: true });
+	spaceBar.release = () => changePressingState({ ...getPressingState(), up: false });
 	leftArrow.press = () => changePressingState({ ...getPressingState(), left: true });
 	leftArrow.release = () => changePressingState({ ...getPressingState(), left: false });
 	rightArrow.press = () => changePressingState({ ...getPressingState(), right: true });
