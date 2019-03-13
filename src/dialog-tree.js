14084b28fa4aa52bce6efb3d7cca7ba43c9a3b8d
Payton Swick <payton@foolord.com>
2019-03-13 18:04:08 -0400

    Clean up some of the dialog

--- src/dialog-tree.js
+++ src/dialog-tree.js
@@ -3,8 +3,7 @@
 export function getDialogTree() {
 	return {
 		explodedShip: {
-			text:
-				"As the ship's hull tears apart and the coldness of space covers your skin, you reflect that at least you were free.",
+			text: "As the ship's hull tears apart, you reflect that at least, at the end, you were free.",
 			action: { type: 'EVENT_GAME_OVER' },
 		},
 		firstLandingNotDone: {
@@ -14,16 +13,16 @@ export function getDialogTree() {
 		},
 		firstLanding1: {
 			text:
-				"In the dingy space port, you are approached by a teenage girl. She's very confident, but always looking over her shoulder. You don't see anyone other than a drunk and a traffic controller nearby, but on this tiny planet you wouldn't be surprised if someone meant this woman harm.",
+				"In the dingy space port, you are approached by a young woman. She's very confident, but always looking over her shoulder. You don't see anyone other than a drunk and a traffic controller nearby, but on this tiny planet you wouldn't be surprised if someone meant this woman harm.",
 			options: [{ text: 'Continue', link: 'firstLanding2' }],
 			action: { type: 'EVENT_FIRST_LANDING' },
 		},
 		firstLanding2: {
 			text:
-				"Girl: You're a captain, right? I'm the one who sent the message. I... I want to buy passage. Passage ... just passage out of here. Please?",
+				"Young woman: You're a captain, right? I'm the one who sent the message. I... I want to buy passage. Passage ... just passage out of here. Please?",
 			options: [
 				{
-					text: 'Not a problem, miss. So long as you can pay.',
+					text: 'Not a problem, friend. So long as you can pay.',
 					link: 'firstLanding3',
 				},
 				{ text: 'What are you doing way out here?', link: 'firstLanding4' },
@@ -31,7 +30,7 @@ export function getDialogTree() {
 		},
 		firstLanding3: {
 			text:
-				"Thanks so much, captain. I won't be trouble, I promise. Just take me to Cassis and I can find my way from there. My name is Alana.",
+				"Young woman: Thanks so much, captain. I won't be trouble, I promise. Just take me to Cassis and I can find my way from there. My name is Alana.",
 			options: [
 				{
 					text: 'Depart',
@@ -40,7 +39,7 @@ export function getDialogTree() {
 		},
 		firstLanding4: {
 			text:
-				'Some people are trying to... they want to bring me somewhere terrible. And I need your help to get away. Everyone said you could help, when people need it. I have the cash. I can pay for my passage.',
+				'Young woman: Some people are trying to... they want to bring me somewhere terrible. And I need your help to get away. Everyone said you could help, when people need it. I have the cash. I can pay for my passage.',
 			options: [
 				{
 					text: "No need to worry. Let's get you where you need to go.",
