42f1ce98c720b5e8f51062800a9632f08ebd7df8
Payton Swick <payton@foolord.com>
2019-03-15 22:48:24 -0400

    Add getEvent and bool to grammar

--- src/parser/grammar.js
+++ src/parser/grammar.js
@@ -7,16 +7,19 @@ const moo = require('moo');
 const lexer = moo.compile({
 	ws: /[ \t]+/,
 	number:  /[-]?(?:0|[1-9][0-9]*)/,
+	bool: ['true', 'false'],
 	string:  /'[^']*'/,
-	command: ['getNpcHappiness'],
+	command: ['getNpcHappiness', 'getEvent'],
 	nl: { match: /\n/, lineBreaks: true },
 	comparator: ['>=', '>', '<', '=', '<='],
 });
 var grammar = {
     Lexer: lexer,
     ParserRules: [
-    {"name": "expression", "symbols": [(lexer.has("command") ? {type: "command"} : command), (lexer.has("ws") ? {type: "ws"} : ws), "unquotedstring", (lexer.has("ws") ? {type: "ws"} : ws), (lexer.has("comparator") ? {type: "comparator"} : comparator), (lexer.has("ws") ? {type: "ws"} : ws), (lexer.has("number") ? {type: "number"} : number)], "postprocess": ( [ command, , key, , comparator, , number ] ) => ({ command, key, comparator, number })},
-    {"name": "unquotedstring", "symbols": [(lexer.has("string") ? {type: "string"} : string)], "postprocess": ( [ quoted ] ) => ({type: 'string', value: quoted.value.replace(/[']/g, '')})}
+    {"name": "expression", "symbols": [(lexer.has("command") ? {type: "command"} : command), (lexer.has("ws") ? {type: "ws"} : ws), "unquotedstring", (lexer.has("ws") ? {type: "ws"} : ws), (lexer.has("comparator") ? {type: "comparator"} : comparator), (lexer.has("ws") ? {type: "ws"} : ws), "rightside"], "postprocess": ( [ command, , key, , comparator, , rightside ] ) => ({ command, key, comparator, rightside })},
+    {"name": "unquotedstring", "symbols": [(lexer.has("string") ? {type: "string"} : string)], "postprocess": ( [ quoted ] ) => ({type: 'string', value: quoted.value.replace(/[']/g, '')})},
+    {"name": "rightside", "symbols": [(lexer.has("number") ? {type: "number"} : number)], "postprocess": ( [ rightside ] ) => rightside},
+    {"name": "rightside", "symbols": [(lexer.has("bool") ? {type: "bool"} : bool)], "postprocess": ( [ rightside ] ) => rightside}
 ]
   , ParserStart: "expression"
 }
--- src/parser/grammar.ne
+++ src/parser/grammar.ne
@@ -3,13 +3,17 @@ const moo = require('moo');
 const lexer = moo.compile({
 	ws: /[ \t]+/,
 	number:  /[-]?(?:0|[1-9][0-9]*)/,
+	bool: ['true', 'false'],
 	string:  /'[^']*'/,
-	command: ['getNpcHappiness'],
+	command: ['getNpcHappiness', 'getEvent'],
 	nl: { match: /\n/, lineBreaks: true },
 	comparator: ['>=', '>', '<', '=', '<='],
 });
 %}
 @lexer lexer
 
-expression -> %command %ws unquotedstring %ws %comparator %ws %number {% ( [ command, , key, , comparator, , number ] ) => ({ command, key, comparator, number }) %}
+expression -> %command %ws unquotedstring %ws %comparator %ws rightside {% ( [ command, , key, , comparator, , rightside ] ) => ({ command, key, comparator, rightside }) %}
 unquotedstring -> %string {% ( [ quoted ] ) => ({type: 'string', value: quoted.value.replace(/[']/g, '')}) %}
+rightside ->
+		%number {% ( [ rightside ] ) => rightside %}
+	| %bool {% ( [ rightside ] ) => rightside %}
