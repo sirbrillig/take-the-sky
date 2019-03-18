fd96abdefdfc83ee46050aa9cc9d61273b9d7e62
Payton Swick <payton@foolord.com>
2019-03-18 13:37:10 -0400

    Remove minification for development

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@
 
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 
 export default {
 	input: 'src/main.js',
@@ -13,5 +12,5 @@ export default {
 	watch: {
 		clearScreen: false,
 	},
-	plugins: [resolve(), commonjs(), terser()],
+	plugins: [resolve(), commonjs()],
 };
