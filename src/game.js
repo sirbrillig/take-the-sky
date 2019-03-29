c836acaef14353944b0b3c7e49f17413ef8bca38
Payton Swick <payton@foolord.com>
2019-03-28 23:32:32 -0400

    Add Physics.isTouching

--- src/game.js
+++ src/game.js
@@ -9,7 +9,6 @@ import {
 } from './math';
 import Vector from './vector';
 import { getPlanetsInSystem, getStarsInSystem } from './planets';
-import { doSpritesOverlap } from './sprites';
 import debugFactory from './debug';
 import reducer from './state-reducer';
 import Dialog from './dialog';
@@ -86,6 +85,47 @@ class Physics {
 		this.accelerationRate = 0.04;
 		this.rotationRate = 0.06;
 		this.maxVelocity = 3;
+		this.hitBox = new Vector(0, 0);
+	}
+
+	isTouching(physics) {
+		// Find the center points of each sprite
+		const r1CenterX = this.position.x;
+		const r1CenterY = this.position.y;
+		const r2CenterX = physics.position.x;
+		const r2CenterY = physics.position.y;
+
+		// Allow using hitBox property instead of width/height if it exists
+		const r1Width = this.hitBox.x;
+		const r1Height = this.hitBox.y;
+		const r2Width = physics.hitBox.x;
+		const r2Height = physics.hitBox.y;
+
+		// Find the half-widths and half-heights of each sprite
+		const r1HalfWidth = r1Width / 2;
+		const r1HalfHeight = r1Height / 2;
+		const r2HalfWidth = r2Width / 2;
+		const r2HalfHeight = r2Height / 2;
+
+		// Calculate the distance vector between the sprites
+		const vx = r1CenterX - r2CenterX;
+		const vy = r1CenterY - r2CenterY;
+
+		// Figure out the combined half-widths and half-heights
+		const combinedHalfWidths = r1HalfWidth + r2HalfWidth;
+		const combinedHalfHeights = r1HalfHeight + r2HalfHeight;
+
+		// Check for a collision on the x axis
+		if (Math.abs(vx) < combinedHalfWidths) {
+			// A collision might be occurring. Check for a collision on the y axis
+			if (Math.abs(vy) < combinedHalfHeights) {
+				// There's definitely a collision happening
+				return true;
+			}
+			// There's no collision on the y axis
+			return false;
+		}
+		return false;
 	}
 
 	handleInput() {}
@@ -246,15 +286,7 @@ class Player extends SpaceThing {
 	update({ currentMap }) {
 		this.physics.update(this);
 		this.sprite.update({ currentMap, player: this });
-		if (
-			currentMap.stars.find(star =>
-				// TODO: make a version of this function which can take physics instances
-				doSpritesOverlap(
-					{ x: star.physics.position.x, y: star.physics.position.y, hitBox: star.physics.hitBox },
-					{ x: this.physics.position.x, y: this.physics.position.y, hitBox: this.physics.hitBox }
-				)
-			)
-		) {
+		if (currentMap.stars.find(star => this.physics.isTouching(star.physics))) {
 			this.health.decrease(4);
 		}
 	}
